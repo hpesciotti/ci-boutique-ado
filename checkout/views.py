d58c939887552a4be30cdabc1f1605cf19ad2b32
Henrique Pesciotti <hpesciotti@gmail.com>
2024-11-06 18:24:41 +0000

    add stripe as payment system

--- checkout/views.py
+++ checkout/views.py
@@ -11,11 +11,13 @@ def checkout(request):
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
-    
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QHr4UIJfcLvaiK6UM6DcSma03ZoiQNs0g5pL3PzsxnqeIp3x3mD3xLHogGtvU5jRYwCuG4am5IFDwix8XcyeRrS003pU2kkoz',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)

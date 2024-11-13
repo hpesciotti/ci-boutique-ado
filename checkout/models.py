b41d00800fbd116218d1910296a0964f9975f28b
Henrique Pesciotti <hpesciotti@gmail.com>
2024-11-13 18:57:28 +0000

    add profile model, custom css and html

--- checkout/models.py
+++ checkout/models.py
@@ -3,14 +3,17 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
+
 from django_countries.fields import CountryField
+
 from products.models import Product
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
-    """
-    """
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
@@ -60,8 +63,6 @@ class Order(models.Model):
 
 
 class OrderLineItem(models.Model):
-    """
-    """
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL

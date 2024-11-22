682dfd2b93cea214605e566a2f27652d73d1f6ce
Henrique Pesciotti <hpesciotti@gmail.com>
2024-11-22 18:22:41 +0000

    add forms.py and add_product to products

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Product, Category
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -60,7 +61,6 @@ def all_products(request):
     return render(request, 'products/products.html', context)
 
 
-
 def product_detail(request, product_id):
     """ 
     A view to show a individual specific product 
@@ -70,3 +70,14 @@ def product_detail(request, product_id):
         'product': product,
         }
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)

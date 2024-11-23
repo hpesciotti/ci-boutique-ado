cd80fcab8c7d173f931caa9cb05f7eac729eaa18
Henrique Pesciotti <hpesciotti@gmail.com>
2024-11-23 19:50:33 +0000

    Add login required and superuser requirement to the products and profiles views

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
 from .forms import UserProfileForm
 
 from checkout.models import Order
 
+@login_required
 def profile(request):
     """ Display the user's profile. """
     
@@ -33,7 +35,7 @@ def profile(request):
 
     return render(request, template, context)
 
-
+@login_required
 def order_history(request, order_number):
     """
     Adds order history to user profile

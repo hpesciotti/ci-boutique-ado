db0da8eec42986d89f269052b7f9c568981f6eae
Henrique Pesciotti <hpesciotti@gmail.com>
2024-11-24 19:31:13 +0000

    Add secret key as environmental variable

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2fsmnsnzexf$=1=i&&yjq2%4*ryy6wf(u*vtrhlv_so(-7gkhp'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
